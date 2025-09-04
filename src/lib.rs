d0412a20dbf821345b1ccf782786bd7e95fdfffe
Mutekichi <ppwatanabe19568@gmail.com>
2025-09-04 16:12:23 +0900

    outline

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod api;
+pub mod internal;
+
+pub mod prelude {
+    pub use crate::api::*;
+    pub use crate::internal::*;
+}
---
+++ tests/local_import.rs
@@ -0,0 +1,6 @@
+use stabilizer_ch_form_rust::prelude::*;
+
+#[test]
+fn test_local_import() {
+    let _ = StabilizerCHForm::new(3);
+}
