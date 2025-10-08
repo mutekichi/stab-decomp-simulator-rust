3cde5bd4d218df4855147b48b6d7bc2dc5e7883c
Mutekichi <ppwatanabe19568@gmail.com>
2025-10-08 11:46:30 +0900

    remove QuantumCircuit.clbits

--- src/circuit/mod.rs
+++ src/circuit/mod.rs
@@ -8,16 +8,13 @@ pub use parser::*;
 
 pub struct QuantumCircuit {
     pub num_qubits: usize,
-    // pub num_cbits: Option<usize>,
     pub gates: Vec<QuantumGate>,
 }
 
 impl QuantumCircuit {
-    // pub fn new(num_qubits: usize, num_cbits: Option<usize>) -> Self {
     pub fn new(num_qubits: usize) -> Self {
         Self {
             num_qubits,
-            // num_cbits,
             gates: Vec::new(),
         }
     }
